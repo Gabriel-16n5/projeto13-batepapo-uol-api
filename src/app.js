8932e2d82adbc7168c9a5e13f93814807943a3da
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-14 16:29:14 -0300

    Todos os requisitos de armazenamento  e configurações dos dados feito

--- src/app.js
+++ src/app.js
@@ -9,5 +9,13 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
+const mongoCLient = new MongoClient(process.env.DATABASE_URL)
+    try{
+        await mongoCLient.connect()
+        console.log("conexão com db feita")
+    }catch (erro) {
+        console.log(erro.message)
+    }
+const db = mongoCLient.db();
 
 app.listen(5000, console.log("Servidor rodando na porta 5000"))
